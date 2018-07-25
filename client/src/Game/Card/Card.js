7d3d4db4889e4c9e4cefab7c39643ceeb4043658
Piosuke <Piosuke@Piosukes-MacBook-Pro.local>
2018-07-24 21:15:28 -0400

    aded axios and homepage pictures

--- client/src/Game/Card/Card.js
+++ client/src/Game/Card/Card.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
+import axios from "axios";
 import './Card.css';
 
 export default class Card extends Component {
-    render() {
+    constructor() {
+        super()
+        this.state ={ card: null}
+    }
+
+    generateImageNumber() {
+        let imgNumber = Math.floor(Math.random() * 61) + 1;
+        return imgNumber;
+    }
+    componentWillMount() {   
+        axios.get("https://api.guildwars2.com/v2/pets/" + this.generateImageNumber())
+        .then(data => {
+            this.setState({
+                card: data.data.icon
+            })
+        })   
+    }
     
-    return(
-            <div className='card'>
+    render() {
+        return(
+            <div className="card">
+                <div className="cardImage" style={{backgroundImage: `url(${this.state.card})`}}>
                 <div className="numbers">
                     <div>1</div>
                     <div>2 3</div>
                     <div>4</div>
                 </div>
+                </div>
             </div>
+        
         )
     }
 }
